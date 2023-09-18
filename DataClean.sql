e77763d0c1bc8086d95ef73dfdfedc8dd2d64805
Otto Schenk <schenkotto1@gmail.com>
2023-09-17 20:53:57 -0500

    Update DataClean.sql

--- DataClean.sql
+++ DataClean.sql
@@ -1,12 +1,10 @@
--- 1. Remove Rows with Missing Values
--- Delete rows where title is NULL
+-- Remove Rows with Missing Values
 DELETE FROM CombinedData WHERE Title IS NULL;
 
 -- Delete rows where the answer is NULL
 DELETE FROM CombinedData WHERE AnswerBody IS NULL;
 
--- 2. Remove Duplicate Rows
--- Assuming that a combination of QuestionId, AnswerId, and Tag defines a unique row
+-- Remove Duplicates
 DELETE FROM CombinedData
 WHERE rowid NOT IN (
     SELECT MIN(rowid)
@@ -14,34 +12,27 @@ WHERE rowid NOT IN (
     GROUP BY Id, AnswerId, Tag
 );
 
--- 3. Trim Whitespace
--- Remove leading and trailing whitespaces from text columns
+-- Trim
 UPDATE CombinedData
 SET Title = TRIM(Title),
     Body = TRIM(Body),
     AnswerBody = TRIM(AnswerBody);
 
--- 4. Normalize Text Data
--- Convert text fields to lowercase for consistency
+-- Normalize Date
 UPDATE CombinedData
 SET Title = LOWER(Title),
     Body = LOWER(Body),
     AnswerBody = LOWER(AnswerBody);
 
--- 5. Filter Out Irrelevant Data
--- Delete rows where the question score is less than 0
+-- Filter Scores below 0
 DELETE FROM CombinedData WHERE Score < 0;
 
--- 6. Check for Date Inconsistencies and Rectify Them
--- Ensure closed dates are after creation dates
+-- Filter Bad Date Data points
 UPDATE CombinedData
 SET ClosedDate = NULL
 WHERE ClosedDate IS NOT NULL AND ClosedDate < CreationDate;
 
--- 7. Handle Duplicate Tags (assuming a question can have multiple tags)
--- This will create a comma-separated list of tags for each question
--- This assumes that there's no question with multiple identical tags.
--- If not, a more complex deduplication should be used.
+-- Handle Duplicate Tags 
 WITH CTE_Tags AS (
     SELECT Id, GROUP_CONCAT(Tag, ',') as AggregatedTags
     FROM CombinedData
